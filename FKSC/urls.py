5fdcc5dfcd432c4cbe17f82e36f3521e7c765729
soonsam123 <karatesoon@gmail.com>
2017-08-06 23:31:27 -0300

    Trying to upload files

--- FKSC/urls.py
+++ FKSC/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     url(r'^', include('inicial.urls')),
 ]
 
-# if settings.DEBUG:
-    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
